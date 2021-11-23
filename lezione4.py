202688cc6a09a36fc8efc2f352dbc038072033c0
Michele <77193519+HyperEmme@users.noreply.github.com>
2021-11-23 19:49:15 +0000

    Aggiunta esercizio lezione4.py dove utilizzo le liste

---
+++ lezione4.py
@@ -0,0 +1,33 @@
+
+class CSVFile():
+
+  def __init__(self, name):
+    self.name=name
+  
+  def get_data(self):
+    my_file = open('shampoo_sales.txt', 'r') 
+    
+    for line in my_file:
+      elements = line.split(',')
+      if elements[0] != 'Date':
+        date  = elements[0]
+        value = elements[1]
+        #valori_prezzo.append(float(value))
+        #valori_data.append(date)
+        valori.append(float(value))
+        valori.append(date)
+    for i in range(len(valori)):
+      print('{}'.format(valori[i]).split(' , '))
+    #for i in range(len(valori_prezzo)):
+      #print('{}'.format(valori_prezzo[i]))
+    #for i in range(len(valori_data)):
+      #print('{}'.format(valori_data[i]))
+    
+    my_file.close()
+
+valori_prezzo = []
+valori_data = []
+valori = []
+
+fileCSV = CSVFile('a')
+fileCSV.get_data()
